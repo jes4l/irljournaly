699275cbe7cc262856dd578074d1f7ffc62e30c2
jes4l <jesalvadgama@gmail.com>
2026-03-20 14:00:20 +0000

    code: hosting to jesal.wiki configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,6 @@ Rails.application.configure do
   config.assets.quiet = true
   config.action_view.annotate_rendered_view_with_filenames = true
   config.action_controller.raise_on_missing_callback_actions = true
-  config.active_job.queue_adapter = :async
+  config.active_job.queue_adapter = :solid_queue
   config.hosts << "jesal.wiki"
 end
