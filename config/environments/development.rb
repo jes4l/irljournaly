ea3880175dc2ded76f45702bb40cd22ea4131da2
jes4l <jesalvadgama@gmail.com>
2026-03-25 15:56:49 +0000

    code: password reset email sends

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,17 +10,29 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
     config.cache_store = :memory_store
-    config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{2.days.to_i}" }
+    config.public_file_server.headers = { "Cache-Control" => "public, max-age=172800" }
   else
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
 
   config.active_storage.service = :local
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+  
+  config.action_mailer.default_url_options = { host: "jesal.wiki", protocol: "https" }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
